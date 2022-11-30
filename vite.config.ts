ba789632e0c60e53105d1120d33bf5383346b7c4
Yulong Fang <aionv@qq.com>
2022-11-30 22:29:26 +0800

    works

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
 import { defineConfig } from "vite";
 import uni from "@dcloudio/vite-plugin-uni";
+import AutoImport from 'unplugin-auto-import/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [uni()],
+  plugins: [
+    uni(),
+    // https://github.com/antfu/unplugin-auto-import
+    AutoImport({
+      imports: [
+        'vue',
+        'pinia',
+        'uni-app',
+      ],
+      dts: 'src/auto-imports.d.ts',
+      dirs: [
+        'src/composables',
+        'src/stores',
+      ],
+      vueTemplate: true,
+    })],
 });
