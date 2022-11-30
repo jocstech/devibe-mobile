6cee8c21d25499f815cb446617a97682900613bf
Yulong Fang <aionv@qq.com>
2022-11-30 21:26:43 +0800

    add sass

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,8 @@
 import { createSSRApp } from "vue";
 import App from "./App.vue";
-// import { addTabBar } from "./components/dynamic/tabBar"
+
 export function createApp() {
   const app = createSSRApp(App);
-  // addTabBar();
   return {
     app,
   };
